5ed3ca5cd3f73193d0058f2c883c014ee2884295
Sanket Mandavgane <sanketmandavgane673@gmail.com>
2024-08-27 00:06:04 +0530

    Flutter codes

---
+++ 07 21-08-24 Pattern/Problem10.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+
+void main() {
+  int? rows = int.parse(stdin.readLineSync()!);
+  for (int i = 0; i < rows; i++) {
+    int num = i + 1;
+    for (int j = 0; j < rows; j++) {
+      stdout.write(" $num");
+      num++;
+    }
+    print(" ");
+  }
+}
---
+++ 07 21-08-24 Pattern/Problem5.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+
+void main() {
+  for (int i = 4; i >= 1; i--) {
+    for (int j = 1; j <= 3; j++) {
+      stdout.write("$i");
+    }
+    print(" ");
+  }
+}
